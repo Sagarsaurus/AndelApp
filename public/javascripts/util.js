d88878a73d7d9521cf6ee5efffe0cbd79143b036
Sagarsaurus <sagrsccr@gmail.com>
2015-06-12 18:55:14 -0400

    Removed "$" delimeters to allow for more natural commands.  Adding "going through" clause next.

--- public/javascripts/util.js
+++ public/javascripts/util.js
@@ -3,21 +3,21 @@
  */
 function parseCommand() {
     var command = document.getElementById('command').value;
-    if(!command.includes(" $to ")) {
+    if(!command.includes(" to ")) {
         alert("You must specify a destination in order to plan a trip!");
     }
-    else if(command.includes(" $from ")) {
-        var pieceOne = command.split("$from");
+    else if(command.includes(" from ")) {
+        var pieceOne = command.split("from");
         var souce = "";
         var destination = "";
-        if(pieceOne[1].includes(" $to ")) {
-            var pieceTwo = pieceOne[1].split(" $to ");
+        if(pieceOne[1].includes(" to ")) {
+            var pieceTwo = pieceOne[1].split(" to ");
             source = pieceTwo[0];
             destination = pieceTwo[1];
         }
         else {
             source = pieceOne[1];
-            var pieceTwo = pieceOne[0].split(" $to ");
+            var pieceTwo = pieceOne[0].split(" to ");
             destination = pieceTwo[1];
         }
         source = source.trim();
@@ -41,7 +41,7 @@ function parseCommand() {
         });
     }
     else {
-        var destination = command.split(" $to ");
+        var destination = command.split(" to ");
         destination = destination[1];
         destination = destination.trim();
         alert("Destination is: "+destination);
