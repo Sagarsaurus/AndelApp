744c7d3bd8dac7fc51d4111feea9eec2971e51d0
Sagarsaurus <sagrsccr@gmail.com>
2015-06-12 12:26:58 -0400

    Added voice recognition modules and installed semantic.  Going to work on text input first, processing it into data that the javascript can handle in order to start calling the api

--- public/javascripts/voice.js
+++ public/javascripts/voice.js
@@ -1,16 +1,21 @@
 
 function voiceRecognition() {
-    var recognition = new webkitSpeechRecognition();
-
-    recognition.onresult = function (e) {
-        var textarea = document.getElementById('results');
-        for (var i = e.resultIndex; i < e.results.length; ++i) {
-            if (e.results[i].isFinal) {
-                textarea.value += e.results[i][0].transcript;
-            }
-        }
-    };
-
-    // start listening
-    recognition.start();
+    //var recognition = new webkitSpeechRecognition();
+    //console.log(document.createElement('input').webkitSpeech);
+    //if (document.createElement('input').webkitSpeech === undefined) {
+    //    alert("We apologize, speech recognition is not available in your browser at this time.")
+    //} else {
+    //
+    //    recognition.onresult = function (e) {
+    //        var textarea = document.getElementById('results');
+    //        for (var i = e.resultIndex; i < e.results.length; ++i) {
+    //            if (e.results[i].isFinal) {
+    //                textarea.value += e.results[i][0].transcript;
+    //            }
+    //        }
+    //    };
+    //
+    //    // start listening
+    //    recognition.start();
+    //}
 }
